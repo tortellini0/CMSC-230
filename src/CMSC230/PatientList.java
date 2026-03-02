b1cab2062aeffb5a7f11e01a17c330b72f27da82
tortellini0 <231547415+tortellini0@users.noreply.github.com>
2026-03-02 03:39:09 -0500

    before looking things over

--- src/CMSC230/PatientList.java
+++ src/CMSC230/PatientList.java
@@ -4,16 +4,40 @@ public class PatientList {
     private Patient[] patientList;
     private final int maxPatients;
     private int patientAmount;
+    private int indexOfIteration = -1;
+
     public PatientList(int max){
+        if (max <= 0){
+            throw new IllegalArgumentException(
+                "max must be greater than 0"
+            );
+        }
         maxPatients = max;
         patientList = new Patient[maxPatients];
         patientAmount = 0;
     }
 
-    // 0a,1b,2d,3d,3enull,null,null,null,null
-    // d
-    // cases no patient, full patient, mid patient 
+
+    /**
+     * adds a patient to the patientList while maintaining the sort of name then date of birth
+     * @param patient - Patient - patient that is being added
+     * @return - boolean - returns true for a successful add but false for an unsuccessful add
+     */
     public boolean add(Patient patient){
+        return addOrdered(patient);
+    }
+
+    /**
+     * adds a patient to the patientList while maintaining the sort of name then date of birth
+     * @param patient - Patient - patient that is being added
+     * @return - boolean - returns true for a successful add but false for an unsuccessful add
+     */
+    private boolean addOrdered(Patient patient){
+        if (patient == null){
+            throw new IllegalArgumentException(
+                "patient cant be null"
+            );
+        }
         int currentIndex = patientAmount - 1;
         if(patientAmount == maxPatients){
             return false;
@@ -37,7 +61,26 @@ public class PatientList {
     // 1 item
     // 2 item
     // 3 item
-    public Patient binarySearch(PatientIdentity id){
+    /**
+     * uses binary search to find a patient using a patient identity
+     * @param id - PatientIdentity - the identity of the patient that is being found
+     * @return - Patient - returns the Patient if it is found and null if it is not found
+     */
+    public Patient find(PatientIdentity id){
+        return binarySearch(id);
+    }
+
+    /**
+     * uses binary search to find a patient using a patient identity
+     * @param id - PatientIdentity - the identity of the patient that is being found
+     * @return - Patient - returns the Patient if it is found and null if it is not found
+     */
+    private Patient binarySearch(PatientIdentity id){
+        if (id == null){
+            throw new IllegalArgumentException(
+                "id cant be null"
+            );
+        }
         int lower = 0;
         int upper = patientAmount - 1;
         int mid = (upper + lower)/2;
@@ -53,6 +96,28 @@ public class PatientList {
             return patientList[mid];
         }else{
             return null;
+        }
+    }
+
+    /**
+     * initiates the iterator
+     */
+    public void initIteration(){
+        indexOfIteration = 0;
+    }
+    
+    /**
+     * uses the indexOfIteration to return the patient at that index of patientList
+     * @return - Patient - the patient that was at the indexOfIteration index
+     */
+    public Patient next(){
+        if (indexOfIteration == -1){
+            return null;
+        }else if ( (indexOfIteration >= maxPatients) || (patientList[indexOfIteration] == null) ){
+            indexOfIteration = -1;
+            return null;
+        }else{
+            return patientList[indexOfIteration++];
         }
     }
 }
--- test/CMSC230/PatientListTest.java
+++ test/CMSC230/PatientListTest.java
@@ -11,6 +11,50 @@ import org.junit.jupiter.api.Test;
 
 public class PatientListTest {
 
+    @Test
+    void constructorThrowsForInvalidArgument(){
+        Exception exception = assertThrows(
+            IllegalArgumentException.class,
+             () -> {new PatientList(-1);}
+        );
+        assertEquals("max must be greater than 0", exception.getMessage());
+
+    }
+
+    @Test
+    void testIterators(){
+        PatientList list = new PatientList(3);
+        PatientIdentity p1ID = new PatientIdentity(new Name("a", "a"), new Date(1,3,3));
+        Patient p1 = new Patient(p1ID);
+        PatientIdentity p2ID = new PatientIdentity(new Name("a", "a"), new Date(1,4,4));
+        Patient p2 = new Patient(p2ID);
+        PatientIdentity p3ID = new PatientIdentity(new Name("b", "b"), new Date(1,3,3));
+        Patient p3 = new Patient(p3ID);
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+
+        //tests that next returns null before being initiated
+        assertTrue(list.next() == null);
+        list.initIteration();
+
+        //confirms that the iterator does get the correct value.
+        assertTrue(p1.getIdentity().match(list.next().getIdentity()));
+        assertTrue(p2.getIdentity().match(list.next().getIdentity()));
+        assertTrue(p3.getIdentity().match(list.next().getIdentity()));
+        assertTrue(list.next() == null);
+
+    }
+
+    @Test
+    void addOrderedThrowsForInvalidArgument(){
+
+        Exception exception = assertThrows(
+            IllegalArgumentException.class,
+             () -> {new PatientList(5).add(null);}
+        );
+        assertEquals("patient cant be null", exception.getMessage());
+    }
 
     @Test
     void testAddOrdered(){
@@ -23,13 +67,75 @@ public class PatientListTest {
         Patient p3 = new Patient(p3ID);
         PatientIdentity p4ID = new PatientIdentity(new Name("c", "c"), new Date(1,3,3));
         Patient p4 = new Patient(p4ID);
-
+        //confirms that the patients are being added
         assertTrue(list.add(p2));
         assertTrue(list.add(p1));
         assertTrue(list.add(p4));
         assertTrue(list.add(p3));
         assertFalse(list.add(p4));
-        //TODO after iterators are done test the validity of the logic by comparing index by index
 
+        //confirms that the list is sorted
+        Patient[] confirmedList = {p1,p2,p3,p4};
+        list.initIteration();
+        for (int i = 0; i < 4; i++){
+            assertTrue(confirmedList[i].getIdentity().match(list.next().getIdentity()));
+        }
+    }
+
+    @Test
+    void binarySearchThrowsForInvalidArgument(){
+        Exception exception = assertThrows(
+            IllegalArgumentException.class,
+             () -> {new PatientList(5).find(null);}
+        );
+        assertEquals("id cant be null", exception.getMessage());
+    }
+
+    @Test
+    void testBinarySearch(){    
+        PatientList list1 = new PatientList(4);
+        PatientIdentity p1ID = new PatientIdentity(new Name("a", "a"), new Date(1,3,3));
+        Patient p1 = new Patient(p1ID);
+        PatientIdentity p2ID = new PatientIdentity(new Name("a", "a"), new Date(1,4,4));
+        Patient p2 = new Patient(p2ID);
+        PatientIdentity p3ID = new PatientIdentity(new Name("b", "b"), new Date(1,3,3));
+        Patient p3 = new Patient(p3ID);
+        PatientIdentity p4ID = new PatientIdentity(new Name("c", "c"), new Date(1,3,3));
+        Patient p4 = new Patient(p4ID);
+        list1.add(p1);
+        list1.add(p2);
+        list1.add(p3);
+        list1.add(p4);
+
+        //testing with 4 pateints
+        assertTrue(p1ID.match(list1.find(p1ID).getIdentity()));
+        assertTrue(p2ID.match(list1.find(p2ID).getIdentity()));
+        assertTrue(p3ID.match(list1.find(p3ID).getIdentity()));
+        assertTrue(p4ID.match(list1.find(p4ID).getIdentity()));
+        assertTrue(list1.find(new PatientIdentity(new Name("1","2"), new Date(1,1,1))) == null);
+        
+        //testing with 1 patient
+        PatientList list2 = new PatientList(1);
+        list2.add(p1);
+        assertTrue(p1ID.match(list2.find(p1ID).getIdentity()));
+        assertTrue(list2.find(new PatientIdentity(new Name("1","2"), new Date(1,1,1))) == null);
+
+        //testing with 2 patients
+        PatientList list3 = new PatientList(2);
+        list3.add(p1);
+        list3.add(p2);
+        assertTrue(p1ID.match(list3.find(p1ID).getIdentity()));
+        assertTrue(p2ID.match(list3.find(p2ID).getIdentity()));
+        assertTrue(list3.find(new PatientIdentity(new Name("1","2"), new Date(1,1,1))) == null);
+
+        //testing with 3 patients
+        PatientList list4 = new PatientList(3);
+        list4.add(p1);        
+        list4.add(p2);        
+        list4.add(p3);        
+        assertTrue(p1ID.match(list4.find(p1ID).getIdentity()));
+        assertTrue(p2ID.match(list4.find(p2ID).getIdentity()));
+        assertTrue(p3ID.match(list4.find(p3ID).getIdentity()));
+        assertTrue(list4.find(new PatientIdentity(new Name("1","2"), new Date(1,1,1))) == null);
     }
 }
