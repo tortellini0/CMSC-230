c6774723bf5b33f6e17ec539842633bc6df6e7cd
tortellini0 <231547415+tortellini0@users.noreply.github.com>
2026-03-18 12:50:17 -0400

    toCSV method and static makePatient method

--- src/CMSC230/Patient.java
+++ src/CMSC230/Patient.java
@@ -1,4 +1,6 @@
 package CMSC230;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 public class Patient {
     private PatientIdentity identity;
 
@@ -13,5 +15,28 @@ public class Patient {
 
             identity = id;
         }
+    }
+
+    public String toCSV(){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        PatientIdentity identity = getIdentity();
+        String name = identity.getName().fullName();
+        String date = format.format(identity.getDateOfBirth());
+        return name + "," + date;
+    }
+
+    public static Patient makePatient(String line){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        //token {last name, first name, date of birth}
+        String[] tokens = line.split(",");
+        PatientIdentity identity;
+
+        try{
+            identity = new PatientIdentity(new Name(tokens[1],tokens[0]), format.parse(tokens[2]));
+            return new Patient(identity);
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
